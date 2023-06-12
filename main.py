3d72372912069ee764e7d80facf192242e0870e6
Sean Shahkarami <sean.shahkarami@gmail.com>
2023-06-12 10:11:21 -0500

    clean up

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ def main():
     token = os.environ["WAGGLE_TOKEN"]
     # cmd = CommandRunner(token, "https://es.sagecontinuum.org")
 
+    events = get_severe_weather_events()
+
+    if len(events) == 0:
+        print("stopping severe weather event job...")
+    else:
+        print("starting severe weather event job...")
+
+
+def get_severe_weather_events():
     r = requests.get("https://api.weather.gov/alerts/active?area=IL")
     r.raise_for_status()
     data = r.json()
 
-    severe_events = [
+    return [
         f
         for f in data["features"]
         if f["properties"].get("@type") == "wx:Alert"
         and f["properties"].get("severity") == "Severe"
     ]
-
-    if len(severe_events) == 0:
-        print("stopping severe weather event job...")
-    else:
-        print("starting severe weather event job...")
 
 
 if __name__ == "__main__":
